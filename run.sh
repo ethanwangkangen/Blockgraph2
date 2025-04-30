53571ed750e5e6a5c3a4c74516bbc338b2f41dd1
Ethan Wang <ethanwangkangen@gmail.com>
2025-04-30 18:07:59 +0800

    Redid some scripts for convenience

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
-cd ../../
-cd build/scratch/b4mesh
-sudo ./ns3.44-main-default  "$@"
+sudo chmod 777 traces.txt
+sudo chmod 777 ./Traces
+sudo chmod 777 ./Traces/*
+sudo chmod 777 ./results
+sudo chmod 777 ./results/*
+sudo ../../build/scratch/b4mesh/./ns3.44-main-default  "$@" > traces.txt
+cd scripts
+sudo ./creategraphs 10
+cd ..
+xdg-open results
+xdg-open traces.txt
+xdg-open ../../build/scratch/b4mesh/Traces
