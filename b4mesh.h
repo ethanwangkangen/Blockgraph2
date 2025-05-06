c470dd023ae53b07387722f2ab35922f936497fa
Ethan Wang <ethanwangkangen@gmail.com>
2025-05-07 01:20:54 +0800

    Bug fixes and documentation

--- b4mesh.h
+++ b4mesh.h
@@ -75,216 +75,325 @@ class B4Mesh : public Application{
   private:
     /**
      * Callback function to process incoming packets on the socket passed in
-     * parameter. Basically, this function implement de protocol of the
-     * blockgraph application.
+     * parameter. 
      */
     void ReceivePacket(Ptr<Socket> socket);
+
     /**
      * Send packet to the node's ip over an UDP socket.
      */
     void SendPacket(ApplicationPacket& packet, Ipv4Address ip, bool scheduled = false);
+
     /**
-     * Sends a packet to all nodes'
+     * Sends a packet to all nodes.
      */
     void BroadcastPacket(ApplicationPacket& packet, vector<Ipv4Address> v);
-    /* Retunes the type of message serialized in the payload */
+
+    /* Extracts the type of message serialized in the payload, 
+     * eg. CHILDLESSBLOCK_REQ 
+     */
     int ExtractMessageType(const string& msg_payload);
 
   public:
     /**
-     * Setup the application
+     * Setup the application.
      */
-    void SetUp(Ptr<Node> node, vector<Ipv4Address> peers, float txMean);
+    void SetUp(Ptr<Node> node, vector<Ipv4Address> peers, float timeBetweenTxn);
+
     /**
      * Method called at time specified by Start.
      */
     virtual void StartApplication();
+
     /**
      * Method called at time specified by Stop.
      */
     virtual void StopApplication();
+
     /**
-     * This is a pointer to the oracle application installed on the
-     * node i
+     * This is a pointer to the oracle application installed on the node i.
      */
     Ptr<B4MeshOracle> GetB4MeshOracle(int nodeId);
+
     /**
-     * Access to an other node B4Mesh application via its Node ID
+     * Access to an other node's B4Mesh application via its Node ID.
      */
     Ptr<B4Mesh> GetB4MeshOf(int nodeId);
+
     /**
      * Method to get the Ipv4Address of the node.
      */
     Ipv4Address GetIpAddress();
+
     /**
-     * Gets the Id of a node from its Ip Address
+     * Gets the Id of a node from its Ip Address.
      */
     int GetIdFromIp(Ipv4Address ip);
+
     /**
-     * Gets the Ip address of a node from its ID
+     * Gets the Ip address of a node from its ID.
      */
     Ipv4Address GetIpAddressFromId(int id);
 
   public:
     // Transaction methods
+
     /**
-     * Do the treatment of a transaction when received
+     * Check if transaction is in mempool, not in blockgraph, and mempool has space.
+     * If so, add it to the mempool.
      */
     void TransactionsTreatment(Transaction t);
+
     /**
-     * Checks if a transaction is already present in the mempool
+     * Checks if a transaction is already present in the mempool.
      */
     bool IsTxInMempool (Transaction t);
   
     /**
-     * Checks that the mempool of the node is not full
+     * Checks that the mempool of the node is not full.
      */
     bool IsSpaceInMempool();
+
     /**
-     * Updates the Txs Mempool once the Block is added to the BG
+     * Updates the mempool once the Block is added to the BG.
+     * Needs to remove transactions in a Block from the mempool.
      */
     void UpdatingMempool(vector<Transaction> txs, string b_hash);
+
     /**
-     * A recursive method to make the node generate transactions
+     * A recursive method to make the node generate transactions.
+     * Create transaction of variable size, then register the transaction.
+     * After that, schedule a new call to itself after an interval.
      */
     void GenerateTransactions();
 
+    /**
+     * Create the actual transaction from payload, treat the transaction, then send
+     * transaction to all nodes in the group after a delay. 
+     */
     void RegisterTransaction(string payload);
 
+    /**
+     * For all transactions in mempool, broadcast all to all nodes in group again.
+     */
     void RetransmitTransactions();
 
+    /**
+     * At regular intervals, ask for missing blocks (if any) 
+     * and resubmit old transactions.
+     */
     void RecurrentTasks();
 
-    void timer_childless_fct(void);
-
+    
     /**
-     * Send the generated transaction to all node in same group
+     * Send the generated transaction to all node in same group.
      */
-    void SendTransaction(Transaction t);
+    void BroadcastTransaction(Transaction t);
 
   public:
     // Block methods
+
     /**
-     * Do the tratment of a block befor adding it to the blockgraph
+     * Do the treatment of a block before adding it to the blockgraph.
      */
     void BlockTreatment(Block block);
 
-    bool IsBlockInWaitingList( string b_hash);
     /**
      * Checks if the hash of the block received as paremeter is present
-     * in the missing_block_list
+     * in the list of blocks awaiting ancestors
+     */
+    bool IsBlockInWaitingList(string b_hash);
+    
+    /**
+     * Checks if the hash of the block received as paremeter is present
+     * in the list of missing childless blocks
      */
     bool IsBlockInMissingList(string b_hash);
+    
     /**
      * Check if the parents of the received block are known by the node
      */
     bool AreParentsKnown(Block &block);
 
-// NEW FUNCTIONS **************
-    vector<string> GetParentsNotInBG(vector<string> parents);
-    vector<string> GetParentsNotInWL(vector<string> p_notIn_BG);
+    /**
+     * Takes a list of block hashes, and filters, returning only those
+     * associated to blocks that are NOT in blockgraph yet.
+     */
+    vector<string> FilterNodesNotInBG(vector<string> parents); //Used to be GetParentsNotInBG
 
     /**
-     * Get the hashes of unknown parents of the received block
+     * Takes a list of block hashes, and filters, returning only those 
+     * NOT in list of blocks waiting for ancestors.
+     * 6 May 2025: I think this is wrong. should be checking if parents are in missing list.
+     * Going to redo the logic and test.
      */
-    vector<string> GetUnknownParents(vector<string> parents);
-  
+    vector<string> FilterNodesNotInML(vector<string> p_notIn_BG);
+
+    // This function does not seem to be used anywhere?
+    // /**
+    //  * Get the hashes of unknown parents of the received block
+    //  */
+    // vector<string> GetUnknownParents(vector<string> parents);
+    
+    // This function does not seem to be used anywhere?  
+    // /**
+    //  *  Start the fast synchronization process of any node
+    //  *  after receiving a merge block
+    //  */
+    // void SyncNode(vector<Transaction> transactions);
+
     /**
-     *  Start the fast synchronization process of any node
-     *  after receiving a merge block
+     * After merge block is treated, start the sync process.
+     * Treat the childless block hashes, send branch requests.
      */
-    void SyncNode(vector<Transaction> transactions);
     void StartSyncProcedure(vector<Transaction> transactions);
 
-    void SendBranchRequest();
     /**
-     * Updates the missing_block_list. If new block is a missing parent
-     * it will ERASE it form the list
+     * Sends request for missing branch to node that has it.
+     */
+    void SendBranchRequest();
+
+    /**
+     * During block treatment, if received block is in missing list, calls
+     * this function to remove it from missing list.
      */
     void EraseMissingBlock(string b_hash);
+
     /**
-     * Updates the missing_block_list. If new block has missing parents
-     * it will ADD them to the missing_block_list
+     * During block treatment, if received block has missing parents that are also
+     * not reflected in the missing list, update the missing list.
      */
     void UpdateMissingList(vector<string> unknown_p, Ipv4Address ip);
+
     /**
      *  Checks that the parents of the blocks in the waiting list are
      *  all present in the local BG. If so, it add the block to the local
      *  BG and erase it from the waiting list
      */
     void UpdateWaitingList();
+
     /**
      *  Add the block to the local blockgraph
      */
     void AddBlockToBlockgraph(Block block);
+
     /*
-     * This method checks the Mempool state every certain time. If Txs in Mempool are
-     * sufficients, then invok GenerateBlocks();
+     * Only executed by the leader.
+     * Checks the mempool state every certain time. If Txs in Mempool are
+     * sufficient, and has been long enough since last block, then invoke GenerateBlocks();
      */
     bool TestPendingTxs();
 
+    /*
+     * If node is leader, TestPendingTxs() is true and allowed to create blocks,
+     * make call to generate block. Repeat at adjustable interval
+     */
     void TestBlockCreation();
 
+    // Can merge the above 2 functions?
+
+    
     /**
-     * A method that generates a new block when Txs in Mempool are enough to
-     * create a block. -Only Leader -Broadcast block to the local group
-     */
+     * Create a template for a block, which is then passed and returned from merge and regular block.
+     * creating functions to create the respective blocks. 
+     * Then after a delay to simulate processing time (based on size of block). send block to consensus module.
+     */ 
     void GenerateBlocks();
 
+    /*
+     * Return True if there are missing childless hashes.
+     */
     bool IsMergeInProcess();
 
+    /*
+     * Take a blank slate block and turn it into a merge block. 
+     */
     Block GenerateMergeBlock(Block &block);
 
+
+    /*
+     * Take a blank slate block and turn it into a regular block. 
+     */
     Block GenerateRegularBlock(Block &block);
 
+    /*
+     * Return list of transactions based on time stamp
+     */
     vector<Transaction> SelectTransactions();
 
   public:
     // REQUEST BLOCK methods
+
     /**
-     * This method checks every certain time if there still missing blocks in the
-     * missing_block_list. If so, it will ask for them to the node who
-     * send the child block of the missing parent.
+     * Checks if there are still missing blocks in the missing_block_list. 
+     * If so, it will send request for them to the node who
+     * sent the child block of the missing parent [??].
      */
     void Ask4MissingBlocks();
+
     /**
-     * It's the answer of Ask4ParentBlock(). It sends the missing parent of a block
-     * to the receiver.
+     * Send block with that hash to the ip address, in response to an Ask4MissingBlocks()
      */
-    void SendBlockto(string hash, Ipv4Address ip);
+    void SendBlockTo(string hash, Ipv4Address ip);
 
   public:
     //  Change topology methods
+
     /**
-     * Starts the merge process of the leader node.
+     * Only called by the leader. Starts the merge process.
      */
     void StartMerge();
 
-    //void GetNewNodes();
+    /**
+     * Return list of nodes that were not in this partition previously. 
+     */
     vector<pair<int, Ipv4Address>> GetNewNodes ();
 
-    vector<pair<int, Ipv4Address>> GetNodesFromOldGroup ();
     /**
-     * Method executed by the leader when a topology change is detected
-     * This method causes a CHILDLESSBLOCK_REQ
+     * Return list of nodes that were not in this partition previously. 
+     */
+    vector<pair<int, Ipv4Address>> GetNodesFromOldGroup ();
+
+    /**
+     * Leader request for childless blocks. 
      */
     void Ask4ChildlessBlocks();
-    //void Ask4ChildlessBlocks(vector<pair<int, Ipv4Address>> new_nodes);
+
     /**
-     * Send the childless blocks to leader upon a CHILDLESSBLOCK_REQ
+     * Ask4ChildlessBlocks() sets a timer for 6 seconds, once that's done it assumes
+     * the requests have all been responded to. Then schedules call to this function
+     * to allow node to create blocks and create a merge block, then calls the GenerateBlocks()
+     * function.
      */
-    void SendChildlessBlocks(Ipv4Address ip);
+    void timer_childless_fct(void);
+
     /**
-     * The leader checks 4 missing side-chains from followers CHILDLESSBLOCK_REP
-     * If a childless block groupId is unkwnow, leader ask for the whole branch
+     * Non-leader sends the childless block hashes to leader upon request.
+     */
+    void SendChildlessHashes(Ipv4Address ip);
+
+    /**
+     * Called by leader, on message sent by the SendChildlessHashes() function.
+     * For each of the childless hashes sent, treat the childless hash.
+     * Then check if can create a merge block.
      */
     void ProcessChildlessResponse(const string& msg_payload, Ipv4Address ip);
 
-    void RegisterNode(string childless, Ipv4Address ip);
+    /**
+     * TODO: complete this
+     */
+    void ChildlessHashTreatment(string childless_hash, Ipv4Address ip); // Was ChildlessBlockTreatment
 
-    void ChildlessBlockTreatment(string childless, Ipv4Address ip);
-
+    /**
+     * Set conditions to allow merge block creation and make call to generate it early (before time expires)
+     * IFF all the required branches have been collected.
+     */
     void CheckMergeBlockCreation();
+
+    /**
+     * Update the recover_branch with the childless hash mapped to the ip of the sender.
+     */
+    void RegisterNode(string childless, Ipv4Address ip);
 
     /**
      * Send a full branch to a node
@@ -293,31 +402,37 @@ class B4Mesh : public Application{
 
   public:
     // Interfaces with Consensus and Group Management (Topology changes)
+
     /**
      * Get the new group and the nature of a change of topology
      */
     void ReceiveNewTopologyInfo(pair<string, vector<pair<int, Ipv4Address>>> new_group, int natchange);
 
     void UpdateTopologyInfo(uint32_t change, pair<string, vector<pair<int, Ipv4Address>>> new_group);
+
     /**
      *  Send the created block to the consensus layer
      */
     void SendBlockToConsensus(Block b);
+
     /**
      *  The consensus layer notifies the blockgraph protocol of a new commited
      *  block.
      */
     static void RecvBlock(void * b4, Block b);
+
     /**
      * This function send the block received from the consensus to the
      * BlockTreatment function.
      */
     void GetBlock(Block b);
+
     /**
      *  This method is an interface btw consensus and blockgraph.
      *  It notifies the blockgraph protocol of the adoption of a new leader
      */
     static void HaveNewLeader(void * b4);
+
     /*
      *  This function gets the last nature change in the network topology
      *  ex. Lastchange = MERGE
@@ -330,6 +445,7 @@ class B4Mesh : public Application{
     void RecurrentSampling ();
 
     int GetNumTxsGen();
+
     int GetNumReTxs();
 
     /**
@@ -430,12 +546,12 @@ class B4Mesh : public Application{
     double sentPacketSizeTotal; // Total size of all packets sent
     double sentTxnPacketSize; // Total size of all transaction packets sent
 
-    double interArrival;   // the mean time of the generation of a transaction
+    double timeBetweenTxn;   // Mean time between transactions
     double startmergetime;
     double endmergetime;
     double count_pendingTx;  // Counts the number of pending transactions
-    double count_missingBlock;  // Counts the number of successfuly recover missing blocks
-    double count_waitingBlock;  // Counts the number of successfuly added blocks from the waiting list
+    double count_missingBlock;  // Counts the number of successfully recovered missing blocks
+    double count_waitingBlock;  // Counts the number of successfully added blocks from the waiting list
     double total_pendingTxTime;  // The cumulative time that transactions spends in the mempool
     double total_missingBlockTime;  // The cumulative time that blocks' references spends in the missing_block_list
     double total_waitingBlockTime;  // The cumulative time that blocks spends in the waiting list
@@ -454,26 +570,53 @@ class B4Mesh : public Application{
     // private member variables of blockgraph protocol 
     Ptr<Socket> recv_sock;
     Ptr<Node> node;
-    bool running;
-    bool mergeBlock;
-    bool createBlock;
-    bool startMerge;
-    int lastChange;  // Last change nature
+
+    bool running; // True if application is running.
+    bool mergeBlock; // True if node is currently creating a merge block.
+    bool createBlock; // True if node is allowed to create a block now (merge or regular).
+    bool startMerge; // True if merge process has started and not ended.
+
+    int lastChange;  // Last change nature. MERGE or SPLIT
     int lastBlock_creation_time;  // Time of last block creation.
     int change;
-    int lastLeader; // Work in progress
-    string groupId;   // GroupId of a group
-    vector<string> groupId_register;
-    vector<Ipv4Address> peers;  // Ip address of all nodes of the system
-    vector<pair<int, Ipv4Address>> group;   // Nodes belonging to the same group
-    vector<pair<int, Ipv4Address>> previous_group;
-    vector<string> missing_childless; //the hashes of the childless block of the branches to recover
-    map<string, Transaction> pending_transactions;  // mempool of transactions
-    map<string, Block> block_waiting_list; // blocks waiting for their ancestors
-    multimap<string, Ipv4Address> missing_block_list;
-    //multimap<int, string> recover_branch; // Nodo ID - Hash of the childless block
+    int lastLeader; // Leader of the latest block to be treated.
+
+    // Ip address of all nodes of the system.
+    // At index i, node with ip = peers[i] has node id i.
+    vector<Ipv4Address> peers;  
+
+    // GroupId of a group
+    string groupId;   
+
+    // Current group that node is in. Each element is a pair containing Node ID and IP address.
+    vector<pair<int, Ipv4Address>> group;   
+
+    // Previous group that node was in. Each element is a pair containing Node ID and IP address.
+    vector<pair<int, Ipv4Address>> previous_group; 
+
+    // List of id of groups that a node has been a part of.
+    vector<string> groupId_register; 
+
+    // Mempool of transactions. Maps transaction hash to the Transaction.
+    map<string, Transaction> txn_mempool;
+
+    // The hashes of the childless block of the branches to recover.
+    vector<string> missing_childless_hashes; // Was: missing_childless
+
+    // Mapping of childless hashes, and sender address (IP address of block to retrieve it from)
+    // Also includes parent blocks known to be missing, as their child is present in the waiting list.
+    multimap<string, Ipv4Address> missing_block_list; 
+
+    // Blocks that cannot be added to Blockgraph yet as they are waiting for ancestors.
+    // Maps Block hash to the block.
+    map<string, Block> block_waiting_list; 
+
+    // Node ID of node that has the branch of the childless block to send.
+    // Maps childless block hash to node id of node to request branch from.
     multimap<string, int> recover_branch;
+    
     Blockgraph blockgraph;  // The database of blocks
+    
     stringstream debug_suffix;
 };
 #endif
